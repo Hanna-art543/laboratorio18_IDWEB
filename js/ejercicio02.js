c58ad745d7dfea8dca8c9cac96a9f8e0c8a754da
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-28 10:01:07 -0500

    Subir ejercicio 06

--- js/ejercicio02.js
+++ js/ejercicio02.js
@@ -3,8 +3,8 @@ async function cargarUsuario() {
         const respuesta = await fetch("https://jsonplaceholder.typicode.com/users/10");
         const usuario = await respuesta.json();
 
-        console.log("Nombre:", usuario.nombre);
-        console.log("Username:", usuario.name);
+        console.log("Nombre:", usuario.name);
+        console.log("Username:", usuario.username);
         console.log("Email:", usuario.email);
     } catch (error) {
         console.error("Error al cargar el usuario:", error);
