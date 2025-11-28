be4e7601b9a400af9c032b2adf98afdc1837aa5f
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-28 11:39:13 -0500

    Subir ejercicio 11

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,6 +1,6 @@
 const regex = /<\/?[^>]+>/g;
 
-const texto = "<h1>Hola</h1> <h1>Enanos</h1>";
+const texto = "<h1>Hola</h1> <h1>Compas</h1>";
 const limpio = texto.replace(regex, "");
 
 console.log(limpio);
